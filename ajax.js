d0b1fc7324b6566714861a7aff5e818197b88d94
Nafia-aj <143184864+Nafia-aj@users.noreply.github.com>
2023-08-29 13:39:12 +0530

    Initial commit

---
+++ ajax.js
@@ -0,0 +1,17 @@
+fetch('https://www.beyondklasses.com:8443/api/getnooflikesforfile').then((data)=> {
+    // console.log(data);
+    return data.json();
+}).then((completedata)=>{
+    // console.log(completedata);
+    let data1="";
+    completedata.map((values) => {
+        data1 += `<div class="card">
+        <img src="${values.filename}"alt="Art" class="images">
+        <p class="Likes">${values.likes}likes</p>
+        <p class="KidName">${values.kidName}</p>
+        </div>`;
+    });
+    document.getElementById("cards").innerHTML=data1;
+}).catch((err)=>{
+console.log(err);
+})
